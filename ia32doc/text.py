c69b44763549934b8a652489f4cc1232147622c3
Petr Benes <w.benny@outlook.com>
2018-07-18 22:19:35 +0200

    remove test paragraph

--- ia32doc/text.py
+++ ia32doc/text.py
@@ -1,30 +1,5 @@
 from typing import List
 
-test = '''Task Switched — Allows the saving of the x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4
-context on a task switch to be delayed until an x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instruction is
-actually executed by the new task. The processor sets this flag on every task switch and tests it when
-executing x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instructions.
-
-  - If the TS flag is set and the EM flag (bit 2 of CR0) is clear, a device-not-available exception (#NM) is
-    raised prior to the execution of any x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instruction; with the
-    exception of PAUSE, PREFETCHh, SFENCE, LFENCE, MFENCE, MOVNTI, CLFLUSH, CRC32, and POPCNT.
-
-  - If the TS flag is set and the MP flag (bit 1 of CR0) and EM flag are clear, an #NM exception is not raised
-    prior to the execution of an x87 FPU WAIT/FWAIT instruction.
-
-  - If the EM flag is set, the setting of the TS flag has no effect on the execution of x87
-    FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instructions.
-
-    The processor does not automatically save the context of the x87 FPU, XMM, and MXCSR registers on a
-    task switch. Instead, it sets the TS flag, which causes the processor to raise an #NM exception whenever
-    it encounters an x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instruction in the instruction stream for the
-    new task (with the exception of the instructions listed above).
-
-    The fault handler for the #NM exception can then be used to clear the TS flag (with the CLTS instruction)
-    and save the context of the x87 FPU, XMM, and MXCSR registers. If the task never encounters an x87
-    FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4 instruction, the x87 FPU/MMX/SSE/SSE2/SSE3/SSSE3/SSE4
-    context is never saved.'''
-
 
 class DocTextCase(object):
     #
