5659d644090bb5636d1b3615fa6bbb28afff39d5
guye1296 <ezer.guy@gmail.com>
2021-06-21 02:07:44 +0300

    update to 1.0.1

--- ia32doc/processors/python/additional-files/setup.py
+++ ia32doc/processors/python/additional-files/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ia32_python",
-    version="1.0",
+    version="1.0.1",
     description="x86-64 definitions for Python 2/3",
     author="guye1296",
     packages=find_packages(),
