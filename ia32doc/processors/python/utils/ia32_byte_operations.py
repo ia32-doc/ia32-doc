47ad10f2b877de9264fe2772b8c96443010375b9
guye1296 <ezer.guy@gmail.com>
2021-06-21 09:59:42 +0300

    python2 compatibility

--- ia32doc/processors/python/utils/ia32_byte_operations.py
+++ ia32doc/processors/python/utils/ia32_byte_operations.py
@@ -2,37 +2,38 @@ import codecs
 
 
 def bin_str(value, max_size):
-    if isinstance(value, int):
-        result = '{0:b}'.format(value).zfill(max_size)
-        # reverse so indexing will work OK
-        return result[::-1]
-    # Supporting `str` for python2.7 support
-    elif isinstance(value, (str, bytes)):
+    if isinstance(value, bytes):
         as_bytearray = bytearray(value)
         result = "".join(
             "{0:b}".format(uint8).zfill(8) for uint8 in as_bytearray
         )
         # NOTE: must encode to little endian
         return result[::-1]
-    else:
+    try:
+        result = '{0:b}'.format(value).zfill(max_size)
+        # reverse so indexing will work OK
+        return result[::-1]
+    except TypeError:
         raise TypeError("Unsupported type: {}".format(type(value)))
 
 
 def size_in_bits(value):
     if isinstance(value, bool):
         return 1
-    elif isinstance(value, int):
-        return len('{0:b}'.format(value))
-    elif isinstance(value, (str, bytes)):
+    elif isinstance(value, bytes):
         return len(value) * 8
-
-    raise TypeError("invalid type: {}".format(type(value)))
+    try:
+        return len('{0:b}'.format(value))
+    except TypeError:
+        raise TypeError("invalid type: {}".format(type(value)))
 
 
 def size_in_bytes(value):
     if isinstance(value, bool):
         return 1
-    elif isinstance(value, int):
+    elif isinstance(value, bytes):
+        return len(value)
+    try:
         if 0 == value:
             return 1
 
@@ -41,23 +42,21 @@ def size_in_bytes(value):
             total_bytes += 1
             value = value >> 8
         return total_bytes
-    elif isinstance(value, (str, bytes)):
-        return len(value)
-
-    raise TypeError("invalid type: {}".format(type(value)))
+    except TypeError:
+        raise TypeError("invalid type: {}".format(type(value)))
 
 
 def to_bytes(value, minimal_width):
-    if isinstance(value, int):
+    if isinstance(value, bool):
+        as_bytes = b'\01' if value else b'\00'
+    elif isinstance(value, (str, bytes)):
+        as_bytes = value
+    try:
         as_bytes = codecs.decode(
             "{0:x}".format(value).zfill(2 * size_in_bytes(value)),
             'hex'
         )
-    elif isinstance(value, bool):
-        as_bytes = b'\01' if value else b'\00'
-    elif isinstance(value, (str, bytes)):
-        as_bytes = value
-    else:
+    except TypeError:
         raise TypeError("invalid type: {}".format(type(value)))
 
     return as_bytes + b'\x00' * (minimal_width - len(as_bytes))
