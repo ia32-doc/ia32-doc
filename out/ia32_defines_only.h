3ce6f68e949dca415f41c7c59f7df8f1dd804033
jono <26609800+jonomango@users.noreply.github.com>
2022-02-12 20:45:28 -0800

    added 64-bit task state segment

--- out/ia32_defines_only.h
+++ out/ia32_defines_only.h
@@ -5346,6 +5346,26 @@ typedef union {
  * @}
  */
 
+#pragma pack(push, 1)
+typedef struct {
+  uint32_t reserved_0;
+  uint64_t rsp0;
+  uint64_t rsp1;
+  uint64_t rsp2;
+  uint64_t reserved_1;
+  uint64_t ist1;
+  uint64_t ist2;
+  uint64_t ist3;
+  uint64_t ist4;
+  uint64_t ist5;
+  uint64_t ist6;
+  uint64_t ist7;
+  uint64_t reserved_2;
+  uint16_t reserved_3;
+  uint16_t io_map_base;
+} task_state_segment_64;
+#pragma pack(pop)
+
 /**
  * @defgroup vmx \
  *           VMX
